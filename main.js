2a698376885c1e1902bb5a25bc7521c5fb14fb53
Jocelyn Shen <jocelynshen@dhcp-10-29-94-193.dyn.mit.edu>
2021-12-02 16:47:07 -0500

    fix navbar

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function onClick(element) {
 window.onscroll = function() {myFunction()};
 function myFunction() {
     var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
     } else {
         navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
